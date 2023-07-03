3a463ebb0dfb32f84c2a991a495743f969b173f6
martimds <martimtimdias@gmail.com>
2023-07-03 11:48:33 -0300

    Disabled end crystals in overworld

--- src/main/java/net/marmo/notnt/NoTNT.java
+++ src/main/java/net/marmo/notnt/NoTNT.java
@@ -1,9 +1,11 @@
 package net.marmo.notnt;
 
 import org.bukkit.World;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.TNTPrimeEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NoTNT extends JavaPlugin implements Listener {
@@ -19,6 +21,13 @@ public final class NoTNT extends JavaPlugin implements Listener {
     public void onTNTPrime (TNTPrimeEvent e){
         if (e.getBlock().getWorld().getEnvironment().equals(World.Environment.NORMAL)){
             e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamage (EntityDamageEvent dmg){
+        if (dmg.getEntity().getType() == EntityType.ENDER_CRYSTAL && dmg.getEntity().getWorld().getEnvironment().equals(World.Environment.NORMAL)){
+            dmg.setCancelled(true);
         }
     }
 }
