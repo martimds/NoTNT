6bb9b0a6a3f4c6b768cc3696a784e4b7abf1b8e7
martimds <martimtimdias@gmail.com>
2023-07-03 12:43:12 -0300

    Disabled end crystals in overworld

--- src/main/java/net/marmo/notnt/NoTNT.java
+++ src/main/java/net/marmo/notnt/NoTNT.java
@@ -1,19 +1,21 @@
 package net.marmo.notnt;
 
+import org.bukkit.Material;
 import org.bukkit.World;
-import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.TNTPrimeEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class NoTNT extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getLogger().info("TNTs disabled in overworld dimension");
         getServer().getPluginManager().registerEvents(this, this);
     }
 
@@ -25,9 +27,13 @@ public final class NoTNT extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onEntityDamage (EntityDamageEvent dmg){
-        if (dmg.getEntity().getType() == EntityType.ENDER_CRYSTAL && dmg.getEntity().getWorld().getEnvironment().equals(World.Environment.NORMAL)){
-            dmg.setCancelled(true);
+    public void onCrystalPlace (PlayerInteractEvent event) {
+        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+            if (event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.END_CRYSTAL) && event.getPlayer().getWorld().getEnvironment().equals(World.Environment.NORMAL)){
+                if (Objects.requireNonNull(event.getClickedBlock()).getType() == Material.OBSIDIAN || event.getClickedBlock().getType() == Material.BEDROCK) {
+                    event.setCancelled(true);
+                }
+            }
         }
     }
 }
